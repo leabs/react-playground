42e3466c47e91dbd87e50bf3f09d69bb39986704
leabs <stevenleabo@gmail.com>
2020-12-07 16:26:00 -0500

    using if / else if

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import './App.css';
 //import { Component } from 'react';
 // import { Body, Body2 } from './components/Body'
 // import { Header } from "./components/Header";
 // import { Counter } from "./components/Counter";
 import { ImageSlider } from "./components/ImageSlider";
+import { Counter } from "./components/Counter";
+class App extends Component {
+  state = {
+      visible: true,
+      whichComponentToShow: 'ImageSlider'
+  }
+  render(){
 
-function App() {
-  return (
-    <div className="App">
-      {/* <Header title="Hey this is a title" 
-              num={7} 
-              myArr={[10,2,3]} 
-              myFunc={(a,b)=> a+b}
-              myObject={{
-                a: 5,
-                b: 6
-      }} />
-      <Body text="i am cool" text2="hey again" />
-      <Body2 />
-      <Counter initialCount={0} />
-      <Counter initialCount={10} /> */}
-      <ImageSlider />
-    </div>
-  );
+    if (this.state.whichComponentToShow === "ImageSlider") {
+      return (
+      <div className="App">
+        <ImageSlider />
+        <button onClick={() => {
+          this.setState({ whichComponentToShow: "Counter"});
+        }}
+        >Show Counter</button>
+      </div>
+      );
+    } else if (this.state.whichComponentToShow === "Counter"){
+      return (
+        <div className="App">
+          <Counter initialCount={0} />
+        </div>
+      );
+    }
+
+    return null;
+  }
 }
 
 export default App;
